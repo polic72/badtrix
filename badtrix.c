afad30a7f42d432df8d2d23df6682fa39db8d586
polic72 <>
2024-05-22 00:23:59 -0400

    Small tests of the vector.

--- badtrix.c
+++ badtrix.c
@@ -2,9 +2,23 @@
 #include <stdlib.h>
 
 #include "matrix.h"
+#include "vector.h"
 
 
 int main()
 {
+    vector v;
+
+    v.n = 3;
+
+    double temp[v.n];
+    
+    v.values = temp;
+
+    for (unsigned int i = 0; i < v.n; ++i)
+    {
+        printf("%d\n", temp[i]);
+    }
+
     return 0;
 }
